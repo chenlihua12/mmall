734b876cef6b2ef330607a4329fa380b9c018bfe
chenlihua12 <1434096442@qq.com>
2019-12-14 19:20:53 +0800

    last

--- src/main/java/com/mmall/common/ResponseCode.java
+++ src/main/java/com/mmall/common/ResponseCode.java
@@ -1,29 +1,29 @@
 package com.mmall.common;
 
 /**
- * @author: xu gang feng
- * @create: 2019-12-09 22:22
+ * Created by geely
  */
 public enum ResponseCode {
-    SUCCESS(1, "成功"),
-    ERROR(0,"失败"),
-    FAIL(2, "错误"),
-    NEED_LOGIN(3, "需要登陆");
+
+    SUCCESS(0,"SUCCESS"),
+    ERROR(1,"ERROR"),
+    NEED_LOGIN(10,"NEED_LOGIN"),
+    ILLEGAL_ARGUMENT(2,"ILLEGAL_ARGUMENT");
 
     private final int code;
     private final String desc;
 
-    public int getCode() {
-        return code;
-    }
 
-    public String getDesc() {
-        return desc;
-    }
-
-    ResponseCode(int code, String desc) {
+    ResponseCode(int code,String desc){
         this.code = code;
         this.desc = desc;
+    }
+
+    public int getCode(){
+        return code;
+    }
+    public String getDesc(){
+        return desc;
     }
 
 }
